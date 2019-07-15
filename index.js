258423beb97a017ae14e9cd94e5e710c653c1399
Josh Glasson <josh.glasson@tesco.com>
2019-07-15 11:44:04 +0100

    Testing permissions with colour example

--- index.js
+++ index.js
@@ -24,6 +24,40 @@ app.intent('Default Welcome Intent', (conv) => {
   }
 });
 
+// Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
+// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
+app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+  if (!permissionGranted) {
+    // If the user denied our request, go ahead with the conversation.
+    conv.ask(`OK, no worries. What's your favorite color?`);
+    conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+  } else {
+    // If the user accepted our request, store their name in
+    // the 'conv.user.storage' object for future conversations.
+    conv.user.storage.userName = conv.user.name.display;
+    conv.ask(`Thanks, ${conv.user.storage.userName}. ` +
+      `What's your favorite color?`);
+    conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+  }
+});
+
+// Handle the Dialogflow NO_INPUT intent.
+// Triggered when the user doesn't provide input to the Action
+app.intent('actions_intent_NO_INPUT', (conv) => {
+  // Use the number of reprompts to vary response
+  const repromptCount = parseInt(conv.arguments.get('REPROMPT_COUNT'));
+  if (repromptCount === 0) {
+    conv.ask('Which color would you like to hear about?');
+  } else if (repromptCount === 1) {
+    conv.ask(`Please say the name of a color.`);
+  } else if (conv.arguments.get('IS_FINAL_REPROMPT')) {
+    conv.close(`Sorry we're having trouble. Let's ` +
+      `try this again later. Goodbye.`);
+  }
+});
+
+
+
 const expressApp = express().use(bodyParser.json());
 
 expressApp.post('/api/test', app);
