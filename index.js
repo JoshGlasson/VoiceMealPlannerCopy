7d46936278ca12526cae56c6d88f0000097848ca
Josh Glasson <josh.glasson@tesco.com>
2019-07-15 08:48:29 +0100

    Updated layout on index

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var routes = require("./routes/routes.js");
 
 var app = express();
 var port=process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 routes(app);
+
 app.listen(port);
+
 console.log('Server Listening at port'+port);
