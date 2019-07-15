202004f9568eb87d7465e59ccfaa0c1a99acf61c
Josh Glasson <josh.glasson@tesco.com>
2019-07-15 11:19:43 +0100

    Testing asking permission

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ const port = process.env.PORT || 4567;
 const app = dialogflow();
 
 app.intent('Default Welcome Intent', conv => {
-    conv.close('Hello, Azure!');
+  const name = conv.user.storage.userName;
+  if (!name) {
+    // Asks the user's permission to know their name, for personalization.
+    conv.ask(new Permission({
+      context: 'Hi there, to get to know you better',
+      permissions: 'NAME',
+    }));
+  } else {
+    conv.ask(`Hi again, ${name}. What's your favorite color?`);
+  }
 });
 
 const expressApp = express().use(bodyParser.json());
 
-expressApp.post('/', app);
+expressApp.post('/api/test', app);
 expressApp.listen(port);
