0b847f2fa40218e21532e7ec11d841c823a82c14
Josh Glasson <josh.glasson@tesco.com>
2019-07-16 15:01:06 +0100

    name wrong

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.intent('Meal_Planner', (conv, {food}) => {
 });
 
 app.intent('Meal_Planner - no', (conv) => {
-  let foodChoice = conv.data.food.splice(Math.floor(Math.random()*array.length), 1);
+  let foodChoice = conv.data.food.splice(Math.floor(Math.random()*result.length), 1);
   conv.ask("Would you like " + foodChoice[0]);
 });
 
@@ -77,7 +77,7 @@ function mealSearch(conv, food){
   .then(function(result){
     log.info('Result Returned before Conv')
     let foodChoice = []
-    foodChoice = move(result, Math.floor(Math.random()*result.length), array.length -1);
+    foodChoice = move(result, Math.floor(Math.random()*result.length), result.length -1);
     
     conv.ask("Would you like " + foodChoice[0]);
     log.info('After Conv')
