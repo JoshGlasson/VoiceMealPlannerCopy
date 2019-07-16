56cf3a1fe0c4be83a5d2927ae23f19490db09645
Josh Glasson <josh.glasson@tesco.com>
2019-07-16 13:38:56 +0100

    Added logs to scraper

--- realfoodScraper.js
+++ realfoodScraper.js
@@ -1,13 +1,17 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 const url = "https://realfood.tesco.com/search.html?search=";
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({name: "scrape"});
 
 
 exports.scrape = function (searchTerm) {
+  log.info('Start Scrape')
   var recipes = []
   var website = url+searchTerm
   return rp(website)
     .then(function(html){
+      log.info('Add to Array')
       var tempArray = []
       for (let i = 0; i < $('.recipe-link', html).length; i++) {
         if($('.recipe-link',html)[i].attribs.href.substring(0,8) == "/recipes") {
@@ -17,6 +21,7 @@ exports.scrape = function (searchTerm) {
           tempArray = []
         }
       }
+      log.info('Scrape Finished')
       return recipes;
     })
 };
