13ca71423599389946e3242568d51fe2c22e38f8
Josh Glasson <josh.glasson@tesco.com>
2019-07-16 13:03:17 +0100

    Added return

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.intent('actions_intent_NO_INPUT', (conv) => {
 app.intent('Meal_Planner', (conv, {food}) => {
   return realFood.scrape(food)
   .then(function(result){
-    foodArray = result[Math.floor(Math.random() * result.length)]
+    return result[Math.floor(Math.random() * result.length)]
   })
   .then(function(result){
     conv.ask("Would you like " + result[0]);
